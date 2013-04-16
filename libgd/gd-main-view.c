ca1647574c3bc1940a376d4a3598259a3b26935e
Cosimo Cecchi <cosimoc@gnome.org>
2013-04-15 21:16:07 -0400

    main-view: signal changes for range and rubberband selections

--- libgd/gd-main-view.c
+++ libgd/gd-main-view.c
@@ -467,6 +467,8 @@ selection_mode_select_range (GdMainView *self,
       /* no other selected element found, just select the iter */
       do_select_row (self, &iter, TRUE);
     }
+
+  g_signal_emit (self, signals[VIEW_SELECTION_CHANGED], 0);
 }
 
 static gboolean
@@ -624,6 +626,8 @@ on_button_release_event (GtkWidget *view,
 
 	      gtk_tree_path_next (start_path);
 	    }
+
+          g_signal_emit (self, signals[VIEW_SELECTION_CHANGED], 0);
 
 	  gtk_tree_path_free (start_path);
 	  gtk_tree_path_free (end_path);
