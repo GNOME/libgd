7f080984bf6a92212def9fb8aa0f6c0f9979b57e
Ignacio Casal Quinteiro <ignacio.casal@nice-software.com>
2013-07-14 11:43:05 +0200

    Make GdTaggedEntryTag a GObject

--- test-tagged-entry.c
+++ test-tagged-entry.c
@@ -1,11 +1,28 @@
 #include <gtk/gtk.h>
 #include <libgd/gd-tagged-entry.h>
 
+static void
+on_tag_clicked (GdTaggedEntry *entry,
+                GdTaggedEntryTag *tag,
+                gpointer useless)
+{
+  g_print ("tag clicked: %s\n", gd_tagged_entry_tag_get_label (tag));
+}
+
+static void
+on_tag_button_clicked (GdTaggedEntry *entry,
+                       GdTaggedEntryTag *tag,
+                       gpointer useless)
+{
+  g_print ("tag button clicked: %s\n", gd_tagged_entry_tag_get_label (tag));
+}
+
 gint
 main (gint argc,
       gchar ** argv)
 {
   GtkWidget *window, *box, *entry;
+  GdTaggedEntryTag *tag;
 
   gtk_init (&argc, &argv);
 
@@ -16,11 +33,25 @@ main (gint argc,
   gtk_container_add (GTK_CONTAINER (window), box);
 
   entry = GTK_WIDGET (gd_tagged_entry_new ());
+  g_signal_connect(entry, "tag-clicked",
+                   G_CALLBACK (on_tag_clicked), NULL);
+  g_signal_connect(entry, "tag-button-clicked",
+                   G_CALLBACK (on_tag_button_clicked), NULL);
   gtk_container_add (GTK_CONTAINER (box), entry);
 
-  gd_tagged_entry_add_tag (GD_TAGGED_ENTRY (entry), "tag1", "Blah1", "documents-entry-tag", TRUE);
-  gd_tagged_entry_insert_tag (GD_TAGGED_ENTRY (entry), "tag2", "Blah2", "documents-entry-tag", FALSE, -1);
-  gd_tagged_entry_insert_tag (GD_TAGGED_ENTRY (entry), "tag3", "Blah3", "documents-entry-tag", FALSE, 0);
+  tag = gd_tagged_entry_tag_new ("Blah1");
+  gd_tagged_entry_add_tag (GD_TAGGED_ENTRY (entry), tag);
+  g_object_unref (tag);
+
+  tag = gd_tagged_entry_tag_new ("Blah2");
+  gd_tagged_entry_tag_set_has_close_button (tag, FALSE);
+  gd_tagged_entry_insert_tag (GD_TAGGED_ENTRY (entry), tag, -1);
+  g_object_unref (tag);
+
+  tag = gd_tagged_entry_tag_new ("Blah3");
+  gd_tagged_entry_tag_set_has_close_button (tag, FALSE);
+  gd_tagged_entry_insert_tag (GD_TAGGED_ENTRY (entry), tag, 0);
+  g_object_unref (tag);
 
   gtk_widget_show_all (window);
   gtk_main ();
