GNOME/libgd

commit 41820ae96142312ffc0109b65d2adad6f007e1fd

list-view: Don't clear attributes on no model
No point in clearing the existing attributes in set_attributes_from_model
if we are not going to actually set any

libgd/gd-main-list-view.c
@@ -61,12 +61,14 @@ set_attributes_from_model (GdMainListView *self)
   GtkTreeModel *model = gtk_tree_view_get_model (GTK_TREE_VIEW (self));
   GType icon_gtype;
 
+
+  if (!model)
+    return;
+
   gtk_tree_view_column_clear_attributes (self->priv->tree_col, self->priv->pixbuf_cell);
   gtk_tree_view_column_clear_attributes (self->priv->tree_col, self->priv->selection_cell);
   gtk_tree_view_column_clear_attributes (self->priv->tree_col, self->priv->text_cell);
 
-  if (!model)
-    return;
 
   gtk_tree_view_column_add_attribute (self->priv->tree_col, self->priv->selection_cell,
                                       "active", GD_MAIN_COLUMN_SELECTED);

commit cb4eb0a838f7d24834fd26f2ccb31932e7c83014

list-view: Remove extra newline
Fallout from 41820ae96142312ffc0109b65d2adad6f007e1fd

libgd/gd-main-list-view.c
@@ -61,7 +61,6 @@ set_attributes_from_model (GdMainListView *self)
   GtkTreeModel *model = gtk_tree_view_get_model (GTK_TREE_VIEW (self));
   GType icon_gtype;
 
-
   if (!model)
     return;
 